153f8988ac7fa2d58ffe5486adc2206c40f338ab
airmon1101 <research01015@gmail.com>
2025-12-31 20:30:24 +0530

    initial commit

---
+++ Code/app.py
@@ -0,0 +1,66 @@
+# SMS_Spam_Classification
+import streamlit as st
+import pickle
+import string
+import nltk
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+ps = PorterStemmer()
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english'):
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+
+vectorizer = pickle.load(open('/home/dhruv/Desktop/ZTM_ML/ML_Project/Code/vectorizer.pkl', 'rb'))
+model = pickle.load(open('/home/dhruv/Desktop/ZTM_ML/ML_Project/Code/model.pkl', 'rb'))
+
+st.title("Email / SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+
+    # 1. Preprocess
+    transformed_sms = transform_text(input_sms)
+
+    # 2. Vectorize
+    vector_input = vectorizer.transform([transformed_sms])
+
+    # 3. Predict
+    result = model.predict(vector_input)[0]
+
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
